3322f3e4b9b10f65d0e0e5e70a22aed59d80f006
Andrew Leung <andrew@ualberta.net>
2015-01-20 01:04:52 -0500

    adding elpa folder

---
+++ elpa/flymake-ruby-0.8/flymake-ruby-autoloads.el
@@ -0,0 +1,29 @@
+;;; flymake-ruby-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (flymake-ruby-load) "flymake-ruby" "flymake-ruby.el"
+;;;;;;  (21693 58992 307925 892000))
+;;; Generated autoloads from flymake-ruby.el
+
+(autoload 'flymake-ruby-load "flymake-ruby" "\
+Configure flymake mode to check the current buffer's ruby syntax.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("flymake-ruby-pkg.el") (21693 58992 321491
+;;;;;;  98000))
+
+;;;***
+
+(provide 'flymake-ruby-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-ruby-autoloads.el ends here
